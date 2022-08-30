1f567ebf0e1d741fcb78d1dcad7f262f565d90a7
analogdev.eth <dejijolaoluwa@gmail.com>
2022-08-30 14:14:40 +0100

    Add utility functions from upstream dependency

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import tokenlist from "./joe.tokenlist-v2.json";
 import schema from "./schema.tokenlist-v2.json";
 
 export * from "./types";
+export * from './isVersionUpdate';
+export * from './getVersionUpgrade';
+export * from './diffTokenLists';
+export * from './minVersionBump';
+export * from './nextVersion';
+export * from './versionComparator';
 export { schema, tokenlist };
